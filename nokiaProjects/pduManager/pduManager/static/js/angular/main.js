c949be9620099475441ab77fdd27fd2ccd5d6750
slawek <slawekzaba@poczta.fm>
2016-04-14 20:56:55 +0200

    poprawa w tworzenu grup

--- nokiaProjects/pduManager/pduManager/static/js/angular/main.js
+++ nokiaProjects/pduManager/pduManager/static/js/angular/main.js
@@ -5,13 +5,6 @@ pduApp.config(function($interpolateProvider) {
 });
 
 pduApp.controller('mainController', function ($scope) {
-  $scope.groups = [
-    {
-      id: 0,
-      name: "all",
-      pdus: []
-    }
-  ]
 
   $scope.arrayPdu = [
     {
@@ -60,89 +53,37 @@ pduApp.controller('mainController', function ($scope) {
       ]
     }
   ];
-  
-  $scope.selectedGroup = $scope.groups[0];
+
+  $scope.groups = [
+    {
+      id: 0,
+      name: "all",
+      allDevices: angular.copy($scope.arrayPdu)
+    }
+  ]
+
+  $scope.init = function(){
+    $scope.selectGroup(0);
+  }
   $scope.selectGroup = function(id){
-    for(var i = 0; i < $scope.groups.length; i++){
-      if($scope.groups[i].id == id){
-        $scope.selectedGroup = $scope.groups[i];
-        break;
-      }
-    }
-  }
-  $scope.isPduinGroup = function(pduId){
-    if($scope.selectedGroup.name == "all" && $scope.selectedGroup.id ==  0){
-      return true;
-    }
-    for(var i = 0; i < $scope.selectedGroup.pdus.length; i++){
-      if($scope.selectedGroup.pdus[i].pduId == pduId)
-        return true;
-    }
-    return false;
-  }
-  $scope.isSlotinGroup = function(pduId, slotId){
-    if($scope.selectedGroup.name == "all" && $scope.selectedGroup.id ==  0){
-      return true;
-    }
-    for(var i = 0; i < $scope.selectedGroup.pdus.length; i++){
-      if($scope.selectedGroup.pdus[i].pduId == pduId)
-      {
-        index = i;
-        break;
-      }
-    }
-    for(var j = 0; j < $scope.selectedGroup.pdus[index].slots.length; ++j){
-      if($scope.selectedGroup.pdus[index].slots[j] == slotId){
-        return true;
-      }
-    }
-    return false;
-  }
-
-  $scope.isInArrayNewGroup = function(pduId){
-    for(var i = 0; i < $scope.newGroup.properties.length; ++i){
-      if($scope.newGroup.properties[i].pduId == pduId){
-        return i;
-      }
-    }
-    return -1;
-  }
-  $scope.selectPduToGroup = function(pdu, slot){
-    if(slot.selected == true)
-    {
-      index = $scope.isInArrayNewGroup(pdu.id);
-      if(index != -1)
-      {
-        $scope.newGroup.properties[index].slots.push(slot.id);
-      }
-      else
-      {
-          var element = {
-          pduId: pdu.id,
-          slots: [slot.id]
+    if(id == 0){
+      $scope.selectedGroup = $scope.groups[0];
+      for(var i = 0 ; i < $scope.selectedGroup.allDevices.length; ++i){
+        $scope.selectedGroup.allDevices[i].selected = true;
+        for(var j = 0 ; j < $scope.selectedGroup.allDevices[i].arraySlots.length; ++j){
+          $scope.selectedGroup.allDevices[i].arraySlots[j].selected = true;
         }
-        $scope.newGroup.properties.push(element);
       }
     }
-    else
-    {
-      for( var i = 0; i < $scope.newGroup.properties.length; ++i)
-      {
-        for ( var j = 0; j < $scope.newGroup.properties[i].slots.length; ++j)
-        {
-          if($scope.newGroup.properties[i].pduId == pdu.id && $scope.newGroup.properties[i].slots[j] == slot.id)
-          {
-            $scope.newGroup.properties[i].slots.splice(j, 1);
-            if($scope.newGroup.properties[i].slots.length == 0){
-              $scope.newGroup.properties.splice(i, 1);
-            }
-            break;
-          }
+    else{
+      for(var i = 0; i < $scope.groups.length; i++){
+        if($scope.groups[i].id == id){
+          $scope.selectedGroup = $scope.groups[i];
+          break;
         }
       }
     }
   }
-
   $scope.selectLabelAdiingGroup = function(pdu){
     $scope.selectedLabel = pdu.id;
   }
@@ -156,22 +97,24 @@ pduApp.controller('mainController', function ($scope) {
 
   $scope.createEmptyGroup = function(){
     $scope.newGroup = {
+      id: $scope.maxId() + 1,
       allDevices: angular.copy($scope.arrayPdu),
-      name: "",
-      properties: []
+      name: ""
     }
     $scope.selectedLabel = $scope.arrayPdu[0].id;
-
   }
-  $scope.createNewGroup = function(){
-    console.log($scope.newGroup);
-    var element = {
-      id: $scope.maxId() + 1,
-      name: angular.copy($scope.newGroup.name),
-      pdus: angular.copy($scope.newGroup.properties)
+  $scope.addPduToGroup = function(pdu, slot){
+    if(slot.selected == true){
+      pdu.selected = true;
+    }else{
+      pdu.selected = false;
     }
-    $scope.groups.push(element);
   }
+
+  $scope.confirmGroup = function(){
+    $scope.groups.push($scope.newGroup);
+  }
+
   $scope.maxId = function(){
     var max = -1;
     for(var i = 0; i < $scope.groups.length; i++){
