f086b166315a927c3b7e97d23fce3ae842f86091
Kujvinho <kujvinho@gmail.com>
2016-06-10 16:12:51 +0200

    poprawienie grupy(outlety)

--- nokiaProjects/pdu_manager/api/views/Create_Groups.py
+++ nokiaProjects/pdu_manager/api/views/Create_Groups.py
@@ -11,26 +11,31 @@ class Create_Groups(View):
     def post(self,request):
         username = request.GET.get("username")
         group_name = request.GET.get("group_name")
-        data_pdu = json.loads(request.body)
-        ids_pdu = data_pdu.get("idpdus")
-        data_outlets = json.loads(request.body)
-        outl = data_outlets.get("idoutlets")
-        
+        data = json.loads(request.body)
+        ids_pdu = data.get("idpdus")
+        outl = data.get("idoutlets")
+        j = 0
+        i = 0
+
         if User.objects.filter(user_name = username).exists():
             host = User.objects.get(user_name = username)
         else:
             host = User.objects.create(user_name = username)
             host.save()
         group = Group.objects.create(name = group_name ,owner = host)
-        group.save()
 
         for idpdu in ids_pdu:
+            i = i + 1
             pdu = Pdu.objects.get(id = idpdu)
             group.pdus.add(pdu)
 
         for idoutlet in outl:
+            j = j + 1
             outlett = Outlet.objects.get(id = idoutlet)
             group.outlets.add(outlett)
+
+        if i > 0 and j > 0:
+            group.save()
 
         if Group.objects.filter(owner = host, name = group_name).exists():
             return JSONResponse({'Succes' : True})
