117936027ee90ee9c8ab3ebbd1b1b3d80119b2ec
Kujvinho <kujvinho@gmail.com>
2016-06-10 17:17:10 +0200

    sprawdzenie w dodawaniu czy sa jakiekolwiek outlety

--- nokiaProjects/pdu_manager/api/views/Create_Groups.py
+++ nokiaProjects/pdu_manager/api/views/Create_Groups.py
@@ -14,30 +14,27 @@ class Create_Groups(View):
         data = json.loads(request.body)
         ids_pdu = data.get("idpdus")
         outl = data.get("idoutlets")
-        j = 0
-        i = 0
 
-        if User.objects.filter(user_name = username).exists():
-            host = User.objects.get(user_name = username)
-        else:
-            host = User.objects.create(user_name = username)
-            host.save()
-        group = Group.objects.create(name = group_name ,owner = host)
-
-        for idpdu in ids_pdu:
-            i = i + 1
-            pdu = Pdu.objects.get(id = idpdu)
-            group.pdus.add(pdu)
-
-        for idoutlet in outl:
-            j = j + 1
-            outlett = Outlet.objects.get(id = idoutlet)
-            group.outlets.add(outlett)
-
-        if i > 0 and j > 0:
+        if len(ids_pdu) > 0 and len(outl) > 0:
+            if User.objects.filter(user_name = username).exists():
+                host = User.objects.get(user_name = username)
+            else:
+                host = User.objects.create(user_name = username)
+                host.save()
+            group = Group.objects.create(name = group_name ,owner = host)
             group.save()
 
-        if Group.objects.filter(owner = host, name = group_name).exists():
-            return JSONResponse({'Succes' : True})
+            for idpdu in ids_pdu:
+                pdu = Pdu.objects.get(id = idpdu)
+                group.pdus.add(pdu)
+
+            for idoutlet in outl:
+                outlett = Outlet.objects.get(id = idoutlet)
+                group.outlets.add(outlett)
+
+            if Group.objects.filter(owner = host, name = group_name).exists():
+                return JSONResponse({'Succes' : True})
+            else:
+                return JSONResponse({'Succes' : False})
         else:
             return JSONResponse({'Succes' : False})
