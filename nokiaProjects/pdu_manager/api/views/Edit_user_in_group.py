15f7e92dc487cd116b56bb31db54643e4c6351c4
Kujvinho <kujvinho@gmail.com>
2016-06-29 21:35:54 +0200

    dodanie usuwania osob z grup

--- nokiaProjects/pdu_manager/api/views/Edit_user_in_group.py
+++ nokiaProjects/pdu_manager/api/views/Edit_user_in_group.py
@@ -28,3 +28,30 @@ class Edit_user_in_group(View):
                 return JSONResponse({'Succes' : True})
         else:
             return JSONResponse({'Succes' : False})
+
+    def delete(self, request):
+        username = request.GET.get("username")
+        group_name = request.GET.get("group_name")
+
+        if Group.objects.filter(name = group_name).exists():
+            group = Group.objects.get(name = group_name)
+            if User.objects.filter(user_name = username).exists():
+                user = User.objects.get(user_name = username)
+                if group.users.filter(users__in = [user]).exists():
+                    if group.owner.id == user.id:
+                        if len(group.users) > 1:
+                            group.owner = group.users[1]
+                            group.users.remove(user)
+                            return JSONResponse({'Succes' : True})
+                        else:
+                            group.delete()
+                            return JSONResponse({'Succes' : True})
+                    else:
+                        group.users.remove(user)
+                        return JSONResponse({'Succes' : True})
+                else:
+                    return JSONResponse({'Succes' : False})
+            else:
+                return JSONResponse({'Succes' : False})
+        else:
+            return JSONResponse({'Succes' : False})
