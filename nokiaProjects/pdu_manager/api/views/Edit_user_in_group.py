61721958cb686e439b6b6f3af251e1b028502f0c
Kujvinho <kujvinho@gmail.com>
2016-06-29 20:54:47 +0200

    Dodawanie usera do grupy

---
+++ nokiaProjects/pdu_manager/api/views/Edit_user_in_group.py
@@ -0,0 +1,30 @@
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import View
+import json
+
+from JSONResponse import JSONResponse
+
+from api.models import *
+
+class Edit_user_in_group(View):
+
+    def post(self,request):
+        username = request.GET.get("username")
+        group_name = request.GET.get("group_name")
+
+        if Group.objects.filter(name = group_name).exists():
+            group = Group.objects.get(name = group_name)
+            if User.objects.filter(user_name = username).exists():
+                user = User.objects.get(user_name = username)
+                if group.users.filter(users__in = [user]).exists():
+                    return JSONResponse({'Succes' : False})
+                else:
+                    group.users.add(user)
+                    return JSONResponse({'Succes' : True})
+            else:
+                user = User.objects.create(user_name = username)
+                user.save()
+                group.users.add(user)
+                return JSONResponse({'Succes' : True})
+        else:
+            return JSONResponse({'Succes' : False})
