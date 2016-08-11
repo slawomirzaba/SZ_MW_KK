985a5a843c7d5519da1f2e4656135b0d4553797f
slawomirzaba <slawekzaba@poczta.fm>
2016-08-11 13:27:08 +0200

    Pierwszy commit do androida - zrobione wstępne menu do aplikacji

---
+++ nokiaProjects/android_pdu_manager/PduManager/app/src/main/java/com/pdumanager/slawek/pdumanager/MenuActivity.java
@@ -0,0 +1,68 @@
+package com.pdumanager.slawek.pdumanager;
+
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.pdumanager.slawek.pdumanager.fragments.DevicesActivity;
+import com.pdumanager.slawek.pdumanager.fragments.MyGroupsActivity;
+import com.pdumanager.slawek.pdumanager.fragments.PublicGroupsActivity;
+
+public class MenuActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        FragmentManager fragmentManager = getFragmentManager();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        fragmentManager.beginTransaction().replace(R.id.content_frame, new DevicesActivity()).commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        FragmentManager fragmentManager = getFragmentManager();
+        int id = item.getItemId();
+
+        if (id == R.id.nav_devices) {
+            fragmentManager.beginTransaction().replace(R.id.content_frame, new DevicesActivity()).commit();
+        } else if (id == R.id.nav_my_groups) {
+            fragmentManager.beginTransaction().replace(R.id.content_frame, new MyGroupsActivity()).commit();
+        } else if (id == R.id.nav_public_groups) {
+            fragmentManager.beginTransaction().replace(R.id.content_frame, new PublicGroupsActivity()).commit();
+        } else if (id == R.id.nav_log_out) {
+
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ nokiaProjects/android_pdu_manager/PduManager/app/src/main/java/com/pdumanager/slawek/pdumanager/fragments/MyGroupsActivity.java
@@ -0,0 +1,20 @@
+package com.pdumanager.slawek.pdumanager.fragments;
+
+import android.app.Fragment;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pdumanager.slawek.pdumanager.R;
+
+public class MyGroupsActivity extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_my_groups, container, false);
+    }
+}
